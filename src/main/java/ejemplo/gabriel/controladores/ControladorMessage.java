496ef88591c6afa70e0821e542c9986fc6986fb2
gabriel brango <gabriel.brango.mt@correo.usa.edu.co>
2021-11-29 22:03:33 -0500

    actualizamos con CrossOrigin Method

--- src/main/java/ejemplo/gabriel/controladores/ControladorMessage.java
+++ src/main/java/ejemplo/gabriel/controladores/ControladorMessage.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -26,6 +28,8 @@ import org.springframework.web.bind.annotation.PutMapping;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="*",methods={RequestMethod.GET,RequestMethod.POST,
+                                  RequestMethod.PUT,RequestMethod.DELETE})
 public class ControladorMessage {
     
    @Autowired /**Inyectamos la dependencias*/
